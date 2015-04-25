3456cdb8509531024ab4f749c164a17c43ff266c
ahinko-giovannyacevedo <giovanny.acevedo@ahinko.com>
2015-04-25 10:41:42 -0600

    Add Splash page to solution

--- AndroidCrediMax/Resources/Resource.Designer.cs
+++ AndroidCrediMax/Resources/Resource.Designer.cs
@@ -82,6 +82,9 @@ namespace AndroidCrediMax
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int SplashLayout = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
