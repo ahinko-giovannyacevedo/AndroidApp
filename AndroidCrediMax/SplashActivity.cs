906273861b4c623ac1f829c10748775f42cf2246
ahinko-giovannyacevedo <giovanny.acevedo@ahinko.com>
2015-04-25 11:10:10 -0600

    Creando Splash Page

--- AndroidCrediMax/SplashActivity.cs
+++ AndroidCrediMax/SplashActivity.cs
@@ -12,14 +12,18 @@ using Android.Widget;
 
 namespace AndroidCrediMax
 {
-    [Activity(Label = "SplashActivity")]
+    using System.Threading;
+    using Android.App;
+    using Android.OS;
+
+    [Activity(Theme="@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Create your application here
+            Thread.Sleep(1000);
+            //StartActivity();
         }
     }
 }
