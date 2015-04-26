4acab8e0b334d80834704e0d611867e836166b05
ahinko-giovannyacevedo <giovanny.acevedo@ahinko.com>
2015-04-25 19:30:17 -0600

    cambios

--- AndroidCrediMax/Resources/Resource.Designer.cs
+++ AndroidCrediMax/Resources/Resource.Designer.cs
@@ -69,11 +69,44 @@ namespace AndroidCrediMax
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f06000c
+			public const int MyButton = 2131099660;
+			
+			// aapt resource value: 0x7f06000b
+			public const int btnConfirm = 2131099659;
+			
+			// aapt resource value: 0x7f06000a
+			public const int btnNumPad_Cero = 2131099658;
+			
+			// aapt resource value: 0x7f060005
+			public const int btnNumPad_Five = 2131099653;
+			
+			// aapt resource value: 0x7f060004
+			public const int btnNumPad_Four = 2131099652;
+			
+			// aapt resource value: 0x7f060008
+			public const int btnNumPad_Height = 2131099656;
+			
+			// aapt resource value: 0x7f060009
+			public const int btnNumPad_Nine = 2131099657;
+			
 			// aapt resource value: 0x7f060001
-			public const int MyButton = 2131099649;
+			public const int btnNumPad_One = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int btnNumPad_Second = 2131099650;
+			
+			// aapt resource value: 0x7f060007
+			public const int btnNumPad_Seven = 2131099655;
+			
+			// aapt resource value: 0x7f060006
+			public const int btnNumPad_Six = 2131099654;
+			
+			// aapt resource value: 0x7f060003
+			public const int btnNumPad_Three = 2131099651;
 			
 			// aapt resource value: 0x7f060000
-			public const int btnRound = 2131099648;
+			public const int txtKeyWord = 2131099648;
 			
 			static Id()
 			{
@@ -126,11 +159,17 @@ namespace AndroidCrediMax
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int Mono_Android_Theme_Splash = 2131034113;
+			// aapt resource value: 0x7f050003
+			public const int Mono_Android_Theme_Splash = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int Theme_Splash = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int button_confirm = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int button_pad = 2131034113;
 			
 			static Style()
 			{
